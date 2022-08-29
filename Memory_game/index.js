09a637f3adb8534570686995e61b3b5d5fcd1875
DanielKoltze <danielkoltze@yahoo.dk>
2022-08-29 13:10:26 +0200

    færdig version af memory. Kan dog blive bedre med at shuffle board når spillet er færdigt

--- Memory_game/index.js
+++ Memory_game/index.js
@@ -1,89 +1,124 @@
 const grid = document.getElementById('grid')
 const list = ['Pictures/balbasaur','Pictures/caterpie','Pictures/charmander','Pictures/pidgey','Pictures/squirtle','Pictures/weedle']
-
-
+let clickedlist = [];
+let counter = 0;
 const cards = [
     {
         'numberInArray':null,
         'id': 0,
-        'link': 'Pictures/balbasaur'
+        'link': 'Pictures/balbasaur.png'
     },
     {
         'numberInArray':null,
         'id': 0,
-        'link': 'Pictures/balbasaur'
+        'link': 'Pictures/balbasaur.png'
     },
     {
         'numberInArray':null,
         'id': 1,
-        'link': 'Pictures/charmander'
+        'link': 'Pictures/charmander.png'
     },
     {
         'numberInArray':null,
         'id': 1,
-        'link': 'Pictures/charmander'
+        'link': 'Pictures/charmander.png'
     },
     {
         'numberInArray':null,
         'id': 2,
-        'link': 'Pictures/squirtle'
+        'link': 'Pictures/squirtle.png'
     },
     {
         'numberInArray':null,
         'id': 2,
-        'link': 'Pictures/squirtle'
+        'link': 'Pictures/squirtle.png'
     },
     {
         'numberInArray':null,
         'id': 3,
-        'link': 'Pictures/caterpie'
+        'link': 'Pictures/caterpie.png'
     },
     {
         'numberInArray':null,
         'id': 3,
-        'link': 'Pictures/caterpie'
+        'link': 'Pictures/caterpie.png'
     },
     {
         'numberInArray':null,
         'id': 4,
-        'link': 'Pictures/pidgey'
+        'link': 'Pictures/pidgey.png'
     },
     {
         'numberInArray':null,
         'id': 4,
-        'link': 'Pictures/pidgey'
+        'link': 'Pictures/pidgey.png'
     },
     {
         'numberInArray':null,
         'id': 5,
-        'link': 'Pictures/weedle'
+        'link': 'Pictures/weedle.png'
     },
     {
         'numberInArray':null,
         'id': 5,
-        'link': 'Pictures/weedle'
+        'link': 'Pictures/weedle.png'
     }
 ]
 
+
+makeBoard();
 const shuffledArray = cards.sort((a, b) => 0.5 - Math.random());
 
 
+function makeBoard(){
 for (let index = 0; index < cards.length; index++) {
     let img = document.createElement('img')
-    img.src = 'Pictures/front.jpeg'
-    img.id = index;
+    img.setAttribute('src','Pictures/front.jpeg')
+    img.setAttribute('data-id', index);
     grid.appendChild(img)
-    cards[index].numberInArray = index
+    img.addEventListener('click', flipcard)
+    
+    }
+}
+
+function flipcard(){
+    let data = this.getAttribute('data-id')
+    let card = {link:cards[data].link, id:cards[data].id, data:data}
+    clickedlist.push(card)
+    this.setAttribute('src',cards[data].link)
+
+    if(clickedlist.length === 2){
+        
+        const element1 = document.querySelector('[data-id="' + clickedlist[0].data + '"]')
+        const element2 = document.querySelector('[data-id="' + clickedlist[1].data + '"]')
+        
+        setTimeout( () => {
+            if(clickedlist[0].id === clickedlist[1].id){
+                counter++;
+                element1.setAttribute('src','Pictures/white.png');
+                element2.setAttribute('src','Pictures/white.png');
+                element1.removeEventListener('click', flipcard);
+                element2.removeEventListener('click',flipcard);
+            }else{
+                element1.setAttribute('src','Pictures/front.jpeg');
+                element2.setAttribute('src','Pictures/front.jpeg');
+            }  
+            if(counter === cards.length/2){
+                refreshBoard();
+            }
+            clickedlist = [];
+        },800)
+        
+        
+        
+    }
 }
 
 
-const imgTags = document.querySelectorAll('img')
-console.log(imgTags)
-
-
-imgTags.forEach(img => {
-    img.addEventListener('click', (e) => {
-        console.log(e)
-     
-    })
-})
+function refreshBoard(){
+    const elements = document.querySelectorAll('img');
+    elements.forEach(element => {
+        element.setAttribute('src','Pictures/front.jpeg')
+        element.addEventListener('click', flipcard)
+    });
+}
