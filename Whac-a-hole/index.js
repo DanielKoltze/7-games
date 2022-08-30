ad609612e2c00e9a7f122378298d963458c43f93
DanielKoltze <danielkoltze@yahoo.dk>
2022-08-30 10:45:42 +0200

    nu kan man stoppe og starte whac a hole

--- Whac-a-hole/index.js
+++ Whac-a-hole/index.js
@@ -1,20 +1,55 @@
-const grid = document.getElementById('grid')
-const startButton = document.getElementById('start')
+const divs = document.querySelectorAll('.square')
+const timer = document.querySelector('#timer')
+const score = document.querySelector('#score')
+const button = document.querySelector('#start')
+let time = 30;
+let counter = 0;
+let randomNumber
+let isRunning = false
 
-createboard(2);
+function shiftDiv(){
+divs.forEach(div => {
+    div.classList.remove('mole')
+})
+randomNumber = Math.floor(Math.random()*9)
 
-function createboard(amountOfBoxes){
-    for (let index = 0; index < amountOfBoxes; index++) {
-        const div = document.createElement('div')
-        div.setAttribute('data-id',index)
-        grid.appendChild(div)
-        console.log(document)
-    }
+divs[randomNumber].classList.add('mole')
+/*divs[randomNumber].addEventListener('mousedown', () =>{
+    counter++;
+    score.innerHTML = counter
+})*/
 }
 
+divs.forEach(div => {
+    div.addEventListener('click', () => {
+        if(div.id == randomNumber){
+            counter++;
+            score.innerHTML = counter
+        }
+    })
+})
+let shiftInterval;
+let timeInterval;
 
-startButton.addEventListener('click', startGame)
-
-function startGame(){
+button.addEventListener('click', () => {
     
-}
+    if(!isRunning){
+        isRunning = true;
+        button.innerHTML = 'stop'
+    shiftInterval = setInterval(shiftDiv, 700)
+    timeInterval = setInterval( () => {
+        time--;
+        timer.innerHTML = time;
+        if(time === -1){
+            clearInterval(shiftInterval)
+            clearInterval(timeInterval)
+            alert('gz your score was: ' + score.textContent)
+            button.innerHTML = 'start'
+        }
+    }, 1000)
+}else{
+    isRunning = false
+    button.innerHTML = 'start'
+    clearInterval(shiftInterval)
+    clearInterval(timeInterval)
+}})
