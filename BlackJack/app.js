942a13c0a4337b89ad999422fe41880b876f9aa6
DanielKoltze <danielkoltze@yahoo.dk>
2022-09-06 19:15:27 +0200

    added more

--- BlackJack/app.js
+++ BlackJack/app.js
@@ -9,13 +9,42 @@ const dealButton = document.querySelector('.deal')
 const hitButton = document.querySelector('.hit')
 const standButton = document.querySelector('.stand')
 
+const amountOfMoney = document.querySelector('.moneyText')
+const betMoney = document.querySelector('.circleText')
+const betButtons = document.querySelectorAll('.chip')
+const resetButton = document.querySelector('.resetButton')
 
+let gameIsRunning = false;
+
+
+    for (let i = 0; i < betButtons.length; i++) {
+        betButtons[i].addEventListener('click',bet = (e) => {
+            if(parseInt(amountOfMoney.textContent)-listOfBets[i] >= 0 && gameIsRunning === false){
+                let amountOfMoneyPlaceholder = parseInt(amountOfMoney.textContent) - listOfBets[i]
+                let betMoneyPlaceholder = parseInt(betMoney.textContent) + listOfBets[i]
+                amountOfMoney.innerHTML = amountOfMoneyPlaceholder
+                betMoney.innerHTML = betMoneyPlaceholder
+               
+            }
+        
+        
+    })
+}
 
 
 let dealerHand = []
 let playerHand = []
+const listOfBets = [1,5,10,25,50,100]
+function setButtons(){
+    dealButton.addEventListener('click',initialize)
 
 
+    resetButton.addEventListener('click',reset = (e) => {
+        amountOfMoney.innerHTML = parseInt(amountOfMoney.textContent) + parseInt(betMoney.textContent)
+        betMoney.innerHTML = 0
+    })
+}
+setButtons()
 
 class Card{
     constructor(imgString,number,type){
@@ -77,28 +106,43 @@ startButton.addEventListener('click', e => {
 
 
 function initialize(){
+   
+    playerSum.innerHTML = 0
+    dealerSum.innerHTML = 0
+    gameIsRunning = true
+    
     hitButton.addEventListener('click',hit)
-    for (let i = 0; i < 2; i++) {
-        let playerCard = document.createElement('img')
-        let card = createRandomCard()
-        playerHand.push(card)
-        playerCard.src = card.imgString
-        playerCard.classList.add("card")
-        playerContainer.appendChild(playerCard)
-    }
-    let dealerCard1 = document.createElement('img')
-    let dealerCard2 = document.createElement('img')
+    standButton.addEventListener('click',stand)
+    hitDealer()
+    setTimeout(() => {
+        let dealerCard2 = document.createElement('img')
+        dealerCard2.src = "back_of_cards.png"
+        dealerCard2.classList.add('back')
+        dealerContainer.appendChild(dealerCard2)
+    },1000)
+    setTimeout(hit,2000)
+    setTimeout(hit,3000)
+    dealButton.removeEventListener('click',initialize)
+    resetButton.removeEventListener('click',reset)
+   
+        betButtons[0].removeEventListener('click',bet)
+        betButtons[1].removeEventListener('click',bet)
+        betButtons[2].removeEventListener('click',bet)
+        betButtons[3].removeEventListener('click',bet)
+        betButtons[4].removeEventListener('click',bet)
+        betButtons[5].removeEventListener('click',bet)
+        
+    
+}
+
+function hitDealer(){
+    let dealerCard = document.createElement('img')
     let card = createRandomCard()
     dealerHand.push(card)
-    dealerCard1.src = card.imgString
-    dealerCard1.classList.add("card")
-    dealerContainer.appendChild(dealerCard1)
-    dealerCard2.src = "back_of_cards.png"
-    dealerCard2.classList.add('back')
-    dealerContainer.appendChild(dealerCard2)
+    dealerCard.src = card.imgString
+    dealerCard.classList.add("card")
+    dealerContainer.appendChild(dealerCard)
     dealerSum.innerHTML = calculateSum(dealerHand)
-    playerSum.innerHTML = calculateSum(playerHand)
-
 }
 
 
@@ -125,10 +169,6 @@ function calculateSum(array){
 }
 
 
-
-
-
-initialize()
 calculateSum(playerHand)
 
 function hit(e){
@@ -140,8 +180,11 @@ function hit(e){
     playerContainer.appendChild(playerCard)
     let sum = calculateSum(playerHand)
     if(sum > 21){
-        playerSum.innerHTML = "you lost"
+        playerSum.innerHTML = "busted"
         hitButton.removeEventListener('click',hit)
+        standButton.removeEventListener('click',hit)
+        youLost()
+        setTimeout(initializeNew,4000)
         //dealButton.addEventListener('click',initializeNew)
         //setTimeout(initializeNew,4000)
     }else{
@@ -149,10 +192,35 @@ function hit(e){
     }
 }
 function initializeNew(){
-
+    gameIsRunning = false
     dealerHand = []
     playerHand = []
-    playerContainer.innerHTML = ''
-    dealerContainer.innerHTML = ''
+    playerSum.innerHTML = ''
+    dealerSum.innerHTML = ''
+    const allCards = document.querySelectorAll('.card')
+    const back = document.querySelector('.back')
+    back.parentNode.removeChild(back)
+      for (let i = 0; i < allCards.length; i++) {
+       allCards[i].parentNode.removeChild(allCards[i])
+      }
+    dealButton.addEventListener('click',initialize)
+    setButtons()
 
 }
+
+function youLost(){
+    betMoney.textContent = 0 
+}
+
+
+function stand(){
+    const back = document.querySelector('.back')
+    back.classList.add('rotate')
+    //back.style.transform = 'rotatey(-90deg)'
+    setTimeout(()=>{
+        back.parentNode.removeChild(back)
+    },1000)
+    setTimeout(hitDealer,1000)
+    
+}
+
